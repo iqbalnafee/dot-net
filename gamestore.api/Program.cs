ed74473b4cf7213986a61d4a35351fe9faf454f5
Nafees Iqbal <33998031+iqbalnafee@users.noreply.github.com>
2024-03-28 08:15:46 +0100

    new game objcet created in c#

--- gamestore.api/Program.cs
+++ gamestore.api/Program.cs
@@ -1,3 +1,18 @@
+using gamestore.api.Entities;
+
+List<Game> games = new() {
+    new Game()
+    {
+        Id = 1,
+        Name = "Call Of Duty",
+        Genre = "Action",
+        Price = 10.11M,
+        ReleaseDate = new DateTime(2000,2,1),
+        ImageUri = "https://www.placehold.co/100"
+
+    }
+};
+
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
